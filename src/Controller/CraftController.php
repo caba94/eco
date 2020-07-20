49295279f3ae91265447cab70128d7e13ffc6adc
baptiste bouchet <baptiste.bouchet1@gmail.com>
2020-07-20 17:01:20 +0200

    av

--- src/Controller/CraftController.php
+++ src/Controller/CraftController.php
@@ -21,33 +21,33 @@ class CraftController extends AbstractController
      */
     public function addCraft(Request $req, EntityManagerInterface $manager, RawRepository $repoRaw,JobRepository $repoJob)
     {
+        //dump($req);
         $craft = new Craft();
-        if ($req->request->count() > 0) {
-           
-            $craft->setName($req->request->get('craftName'));
-            if ($req->request->get('raw1') != "") {
-                $craft->setRaw1($repoRaw->findOneBy(['name' => $req->request->get('raw1')]));
-                $craft->setQraw1($req->request->get('q1'));
-            }
-            if ($req->request->get('raw2') != "") {
-                $craft->setRaw2($repoRaw->findOneBy(['name' => $req->request->get('raw2')]));
-                $craft->setQraw2($req->request->get('q3'));
-            }
-            if ($req->request->get('raw3') != "") {
-                $craft->setRaw3($repoRaw->findOneBy(['name' => $req->request->get('raw3')]));
-                $craft->setQraw3($req->request->get('q2'));
-            }
-            $craft->setJob($repoJob->findOneBy(['name' => $req->request->get('job')]));
-            
+        
+        $form = $this->createForm(CraftType::class,$craft);
+        
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid()){
             $manager->persist($craft);
             $manager->flush();
+
+            $this->redirectToRoute('item',$craft->getId());
         }
 
-        $form = $this->createForm(CraftType::class,$craft);
         return $this->render('craft/addCraft.html.twig', [
             'raws' => $repoRaw->findAll(),
             'jobs' => $repoJob->findAll(),
             'Craft' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/item/{id}",name="item")
+     */
+    public function itemView(Craft $craft){
+        return $this->render('craft/item.html.twig',[
+            'craft'=> $craft
+        ]);
+    }
+
 }
--- src/Entity/Craft.php
+++ src/Entity/Craft.php
@@ -6,6 +6,7 @@ use App\Repository\CraftRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Asserts;
 
 /**
  * @ORM\Entity(repositoryClass=CraftRepository::class)
@@ -41,16 +42,31 @@ class Craft
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Asserts\Range(
+     *      min = 0,
+     *      max = 10,
+     *      notInRangeMessage = "You must be between {{ min }} and {{ max }} to enter",
+     * )
      */
     private $Qraw1;
    
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Asserts\Range(
+     *      min = 0,
+     *      max = 10,
+     *      notInRangeMessage = "You must be between {{ min }} and {{ max }} tall to enter",
+     * )
      */
     private $Qraw2;
    
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Asserts\Range(
+     *      min = 0,
+     *      max = 10,
+     *      notInRangeMessage = "You must be between {{ min }} and {{ max }} tall to enter",
+     * )
      */
     private $Qraw3;
 
