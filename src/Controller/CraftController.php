f18b406fc09e9ea4e1d161e8c10adf1e16827090
blackchat <baptiste.bouchet1@gmail.com>
2020-08-04 11:07:46 +0200

    add

--- src/Controller/CraftController.php
+++ src/Controller/CraftController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Craft;
 use App\Entity\Raw;
+use App\Entity\RawCraft;
 use App\Form\CraftType;
 use App\Repository\JobRepository;
 use App\Repository\RawRepository;
@@ -23,15 +24,39 @@ class CraftController extends AbstractController
     {
         //dump($req);
         $craft = new Craft();
-        
+        $rawCraft1 = new RawCraft();
+        $craft->getRawCrafts()->add($rawCraft1);
+        $rawCraft2 = new RawCraft();
+        $craft->getRawCrafts()->add($rawCraft2);
+        $rawCraft3 = new RawCraft();
+        $craft->getRawCrafts()->add($rawCraft3);
+
+
         $form = $this->createForm(CraftType::class,$craft);
         
         $form->handleRequest($req);
         if($form->isSubmitted() && $form->isValid()){
-            $manager->persist($craft);
+            $addcraft = new Craft();
+            $addcraft->setName($craft->getName());
+            $addcraft->setJob($craft->getJob());
+            if($rawCraft1->getQuantity() > 0){
+                $rawCraft1->setCraft($addcraft);
+                $manager->persist($rawCraft1);
+            }
+            if($rawCraft2->getQuantity() > 0){
+                $rawCraft2->setCraft($addcraft);
+                $manager->persist($rawCraft2);
+            }
+            if($rawCraft3->getQuantity() > 0){
+                $rawCraft3->setCraft($addcraft);
+                $manager->persist($rawCraft3);
+            }
+
+            
+            $manager->persist($addcraft);
             $manager->flush();
 
-            $this->redirectToRoute('item',['id'=>$craft->getId()]);
+            $this->redirectToRoute('item',['id'=>$addcraft->getId()]);
         }
 
         return $this->render('craft/addCraft.html.twig', [
--- src/Form/RawCraftType.php
+++ src/Form/RawCraftType.php
@@ -2,21 +2,28 @@
 
 namespace App\Form;
 
+use App\Entity\Raw;
 use App\Entity\RawCraft;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class RawCraftType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('quantity')
-            ->add('Raw', CollectionType::class, [
-                'entry_type' => RawType::class,
-                'entry_options' => ['label' => 'name']
-            ]);
+            ->add('Raw', EntityType::class, [
+                'class' => Raw::class,
+                'choice_label' => 'name',
+            ])
+            ->add(
+                'quantity',
+                null,
+                ['attr'=> ['value'=>0]]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
